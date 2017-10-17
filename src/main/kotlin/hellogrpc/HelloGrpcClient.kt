2c7e597f557a99f1434774fee96b3edeb1973794
Dan Lee <dan.lee@papercut.com>
2017-10-17 12:18:26 +1100

    added more examples

--- src/main/kotlin/hellogrpc/HelloGrpcClient.kt
+++ src/main/kotlin/hellogrpc/HelloGrpcClient.kt
@@ -1,34 +1,115 @@
 package hellogrpc
 
+import hello.HelloGrpc
+import hello.HelloProto
 import io.grpc.ManagedChannelBuilder
 import io.grpc.StatusRuntimeException
 
+object Hello1Client {
 
-fun main(args: Array<String>) {
-    val port = 9010
+    fun main(args: Array<String>) {
+        val port = 9010
 
-    val channel = ManagedChannelBuilder.forAddress("127.0.0.1", port)
-            .usePlaintext(true)
-            .build()
+        val channel = ManagedChannelBuilder.forAddress("127.0.0.1", port)
+                .usePlaintext(true)
+                .build()
 
-    val stub = HelloGrpcGrpc.newBlockingStub(channel)
+        val stub = HelloGrpc.newBlockingStub(channel)
 
-    (1 .. 10).forEach { i ->
-        val request = HelloGrpcProto.HelloRequest.newBuilder()
-                .setText("Dan $i")
+        println("hello1 call ---------------------------------")
+        val request = HelloProto.HelloRequest.newBuilder()
+                .setText("Hello")
                 .build()
 
         try {
-            val response = stub.hello(request)
-
-            response.forEach {
-                println("response: $it")
-            }
+            val res = stub.hello1(request)
+            println("response: $res")
 
         } catch (e: StatusRuntimeException) {
             throw IllegalStateException(
                     "Failed to call hello. status: " + e.status + ", message: " + e.message)
         }
     }
+}
 
+object Hello2Client {
+
+    fun main(args: Array<String>) {
+        val port = 9010
+
+        val channel = ManagedChannelBuilder.forAddress("127.0.0.1", port)
+                .usePlaintext(true)
+                .build()
+
+        val stub = HelloGrpc.newFutureStub(channel)
+
+        println("hello2 call ---------------------------------")
+        val request = HelloProto.HelloRequest.newBuilder()
+                .setText("Hello")
+                .build()
+
+        try {
+            stub.hello1()
+            val res = stub.hello1(request)
+            println("response: $res")
+
+        } catch (e: StatusRuntimeException) {
+            throw IllegalStateException(
+                    "Failed to call hello. status: " + e.status + ", message: " + e.message)
+        }
+    }
+}
+
+object Hello3Client {
+
+    fun main(args: Array<String>) {
+        val port = 9010
+
+        val channel = ManagedChannelBuilder.forAddress("127.0.0.1", port)
+                .usePlaintext(true)
+                .build()
+
+        val stub = HelloGrpc.newBlockingStub(channel)
+
+        println("hello3 call ---------------------------------")
+        val request = HelloProto.HelloRequest.newBuilder()
+                .setText("Hello")
+                .build()
+
+        try {
+            val res = stub.hello1(request)
+            println("response: $res")
+
+        } catch (e: StatusRuntimeException) {
+            throw IllegalStateException(
+                    "Failed to call hello. status: " + e.status + ", message: " + e.message)
+        }
+    }
+}
+
+object Hello4Client {
+
+    fun main(args: Array<String>) {
+        val port = 9010
+
+        val channel = ManagedChannelBuilder.forAddress("127.0.0.1", port)
+                .usePlaintext(true)
+                .build()
+
+        val stub = HelloGrpc.newBlockingStub(channel)
+
+        println("hello4 call ---------------------------------")
+        val request = HelloProto.HelloRequest.newBuilder()
+                .setText("Hello")
+                .build()
+
+        try {
+            val res = stub.hello1(request)
+            println("response: $res")
+
+        } catch (e: StatusRuntimeException) {
+            throw IllegalStateException(
+                    "Failed to call hello. status: " + e.status + ", message: " + e.message)
+        }
+    }
 }
--- src/main/kotlin/hellogrpc/HelloGrpcServer.kt
+++ src/main/kotlin/hellogrpc/HelloGrpcServer.kt
@@ -1,15 +1,16 @@
 package hellogrpc
 
+import hello.HelloGrpc
+import hello.HelloProto
 import io.grpc.ServerBuilder
 import io.grpc.stub.StreamObserver
-
 
 
 fun main(args: Array<String>) {
     val port = 9010
 
     val server = ServerBuilder.forPort(port)
-            .addService(HelloGrpServerImpl())
+            .addService(HelloServerImpl())
             .build()
             .start()
 
@@ -19,22 +20,93 @@ fun main(args: Array<String>) {
 }
 
 
+class HelloServerImpl : HelloGrpc.HelloImplBase() {
 
-class HelloGrpServerImpl : HelloGrpcGrpc.HelloGrpcImplBase() {
+    override fun hello1(
+            req: HelloProto.HelloRequest,
+            resObserver: StreamObserver<HelloProto.HelloResponse>
+    ) {
+        println("Received request in hello1: $req")
 
-    override fun hello(request: HelloGrpcProto.HelloRequest,
-                       responseObserver: StreamObserver<HelloGrpcProto.HelloResponse>) {
+        val res = HelloProto.HelloResponse.newBuilder()
+                .setText("Reply Hello1 ${req.text}")
+                .build()
 
-        println("request: $request")
-
-        (1 .. 2).forEach { i ->
-            Thread.sleep(100)
-            val response = HelloGrpcProto.HelloResponse.newBuilder()
-                    .setText("Reply Hello ${request.text}")
-                    .build()
-            responseObserver.onNext(response)
-        }
-        responseObserver.onCompleted()
+        resObserver.onNext(res)
+        resObserver.onCompleted()
     }
-}
 
+
+    override fun hello2(
+            resObserver: StreamObserver<HelloProto.HelloResponse>
+    ): StreamObserver<HelloProto.HelloRequest> {
+
+        return object : StreamObserver<HelloProto.HelloRequest> {
+
+            override fun onNext(req: HelloProto.HelloRequest) {
+                println("Received request in hello2: $req")
+
+                val res = HelloProto.HelloResponse.newBuilder()
+                        .setText("Reply Hello2 ${req.text} from stream request")
+                        .build()
+                resObserver.onNext(res)
+            }
+
+            override fun onError(t: Throwable) {
+                println("Encountered an error in hello2: ${t.message}")
+            }
+
+            override fun onCompleted() {
+                resObserver.onCompleted()
+            }
+        }
+    }
+
+    override fun hello3(
+            req: HelloProto.HelloRequest,
+            resObserver: StreamObserver<HelloProto.HelloResponse>
+    ) {
+
+        println("Received request in hello3: $req")
+
+        (1 .. 5).forEach { it ->
+            val response = HelloProto.HelloResponse.newBuilder()
+                    .setText("Reply Hello3 ${req.text} - stream response: $it")
+                    .build()
+
+            resObserver.onNext(response)
+        }
+
+        resObserver.onCompleted()
+    }
+
+    override fun hello4(
+            responseObserver: StreamObserver<HelloProto.HelloResponse>
+    ): StreamObserver<HelloProto.HelloRequest> {
+
+        return object : StreamObserver<HelloProto.HelloRequest> {
+
+            override fun onNext(req: HelloProto.HelloRequest) {
+
+                println("Received request in hello4: $req")
+
+                (1 .. 5).forEach { it ->
+                    val res = HelloProto.HelloResponse.newBuilder()
+                            .setText("Reply Hello4 ${req.text} - stream response: $it")
+                            .build()
+
+                    responseObserver.onNext(res)
+                }
+            }
+
+            override fun onError(t: Throwable) {
+                println("Encountered an error in hello2: ${t.message}")
+            }
+
+            override fun onCompleted() {
+                responseObserver.onCompleted()
+            }
+        }
+    }
+
+}
